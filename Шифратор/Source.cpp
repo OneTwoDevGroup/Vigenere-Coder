658cc9adb1e713f1c0fe74154f391c280d08571c
Дмитрий Куликов <kulikovdmitriyarkadevich@gmail.com>
2016-04-25 19:00:07 +0300

    Новый шифратор

--- Шифратор/Source.cpp
+++ Шифратор/Source.cpp
@@ -1,12 +1,15 @@
 #include "stdio.h"
 #include "string.h"
+#include "time.h"
+#include "stdlib.h"
 #define  N 26
 int main()
 {
+	srand(time(NULL));
 	FILE *fp1 = fopen("input.txt", "r");
 	FILE *fp2 = fopen("output.txt", "w");
-	char code[100],chbuf,c;
-	int sizefile,sizecode,i,j;
+	char code[100], chbuf, c;
+	int sizefile, sizecode, i, j;
 	printf("enter a keyword\n");
 	sizecode = strlen(code);
 	scanf("%s", code);
@@ -14,19 +17,30 @@ int main()
 	sizefile = ftell(fp1);
 	fseek(fp1, 0, SEEK_SET);
 	i = 0;
-	char alphabet [N] = { 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z' };
+	char alphabet[N] = { 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z' };
+	char savealphabet[N] = { 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z' };
 	char d[127][127];
+	char newalphabet[N];
+	c = N;
 	for (i = 0; i < N; i++)
-	{	
+	{
+		j = rand() % c;
+		newalphabet[i] = alphabet[j];
+		alphabet[j] = alphabet[c - 1];
+		c--;
+	}
+
+	for (i = 0; i < N; i++)
+	{
 		for (j = 0; j < N; j++)
 		{
 
 			c = j + i;
-			if (c > N-1)
+			if (c > N - 1)
 				c -= N;
-			d[i+alphabet[0]][j + alphabet[0]] = alphabet[0] +c;
+			d[savealphabet[i]][savealphabet[j]] = newalphabet[c];
 		}
-			
+
 	}
 
 	i = 0;
@@ -41,14 +55,13 @@ int main()
 			chbuf = d[code[i]][chbuf];
 			i++;
 		}
-		if ((chbuf>64)&&(chbuf<91))
+		if ((chbuf>64) && (chbuf<91))
 		{
 
-			chbuf = d[code[i]][chbuf+32];
+			chbuf = d[code[i]][chbuf + 32];
 			i++;
 		}
 		fputc(chbuf, fp2);
 	}
 	return 0;
 }
-	

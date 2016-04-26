1ed6a125fee99b2b7694766d7849b709577b3fd8
Дмитрий Куликов <kulikovdmitriyarkadevich@gmail.com>
2016-04-26 20:12:57 +0300

    Шифратор использующий смешанный алфавит

--- Шифратор/Source.cpp
+++ Шифратор/Source.cpp
@@ -2,25 +2,29 @@
 #include "string.h"
 #include "time.h"
 #include "stdlib.h"
-#define  N 26
+#include "locale.h"
+#define  N 32
 int main()
-{
+{	
+	setlocale(LC_ALL, "Russian");
 	srand(time(NULL));
 	FILE *fp1 = fopen("input.txt", "r");
 	FILE *fp2 = fopen("output.txt", "w");
-	char code[100], chbuf, c;
+	unsigned char code[100]="ËÈÌÎÍ", chbuf, c;
 	int sizefile, sizecode, i, j;
 	printf("enter a keyword\n");
-	sizecode = strlen(code);
-	scanf("%s", code);
+	sizecode = strlen((const char*)code);
 	fseek(fp1, 0, SEEK_END);
 	sizefile = ftell(fp1);
 	fseek(fp1, 0, SEEK_SET);
 	i = 0;
-	char alphabet[N] = { 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z' };
+	/*char alphabet[N] = { 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z' };
 	char savealphabet[N] = { 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z' };
-	char d[127][127];
-	char newalphabet[N];
+	*/
+	unsigned char alphabet[N] = { 'À','Á','Â','Ã','Ä','Å','Æ','Ç','È','É','Ê','Ë','Ì','Í','Î','Ï','Ð','Ñ','Ò','Ó','Ô','Õ','Ö','×','Ø','Ù','Ý','É','Û','Ü','Þ','ß' };
+	unsigned char savealphabet[N]= { 'À','Á','Â','Ã','Ä','Å','Æ','Ç','È','É','Ê','Ë','Ì','Í','Î','Ï','Ð','Ñ','Ò','Ó','Ô','Õ','Ö','×','Ø','Ù','Ý','É','Û','Ü','Þ','ß' };
+	unsigned char d[256][256];
+	unsigned char newalphabet[N];
 	c = N;
 	for (i = 0; i < N; i++)
 	{
@@ -29,7 +33,7 @@ int main()
 		alphabet[j] = alphabet[c - 1];
 		c--;
 	}
-
+	c = 0;
 	for (i = 0; i < N; i++)
 	{
 		for (j = 0; j < N; j++)
@@ -43,25 +47,35 @@ int main()
 
 	}
 
-	i = 0;
+	i = 0; j = 0;
 	while (sizefile)
 	{
+		if (j == 5)
+		{
+			fputc(' ', fp2);
+			j = 0;
+		}
 		chbuf = fgetc(fp1);
 		sizefile--;
-		if (i == strlen(code))
-			i -= strlen(code);
-		if (chbuf >96)
+		if (i == strlen((const char*) code))
+			i -= strlen((const char*)code);
+		if ((chbuf >191)&&(chbuf<224))
 		{
 			chbuf = d[code[i]][chbuf];
 			i++;
+			fputc(chbuf, fp2);
+			j++;
 		}
-		if ((chbuf>64) && (chbuf<91))
+		if ((chbuf>223))
 		{
 
-			chbuf = d[code[i]][chbuf + 32];
+			chbuf = d[code[i]][chbuf - 32];
 			i++;
+			fputc(chbuf, fp2);
+			j++;
 		}
-		fputc(chbuf, fp2);
+	
+	
 	}
 	return 0;
 }

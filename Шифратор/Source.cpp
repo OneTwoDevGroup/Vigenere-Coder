05ff9a8c4e172ad8eeb5fad35d5cc85a21de9018
Дмитрий Куликов <kulikovdmitriyarkadevich@gmail.com>
2016-05-13 19:29:44 +0300

    Добавление ручного ввода ключа и алфавита

--- Шифратор/Source.cpp
+++ Шифратор/Source.cpp
@@ -10,7 +10,7 @@ int main()
 	srand(time(NULL));
 	FILE *fp1 = fopen("input.txt", "r");
 	FILE *fp2 = fopen("output.txt", "w");
-	unsigned char code[100]="ЛИМОН", chbuf, c;
+	unsigned char code[21]="ЛИМОН", chbuf, c;
 	int sizefile, sizecode, i, j;
 	printf("enter a keyword\n");
 	sizecode = strlen((const char*)code);
@@ -18,64 +18,144 @@ int main()
 	sizefile = ftell(fp1);
 	fseek(fp1, 0, SEEK_SET);
 	i = 0;
+	int change = 0;
+	printf("1. Случайный.\n2.Ввести самому.\n");
+	scanf("%d", &change);
 	/*char alphabet[N] = { 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z' };
 	char savealphabet[N] = { 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z' };
 	*/
+
 	unsigned char alphabet[N] = { 'А','Б','В','Г','Д','Е','Ж','З','И','Й','К','Л','М','Н','О','П','Р','С','Т','У','Ф','Х','Ц','Ч','Ш','Щ','Э','Й','Ы','Ь','Ю','Я' };
 	unsigned char savealphabet[N]= { 'А','Б','В','Г','Д','Е','Ж','З','И','Й','К','Л','М','Н','О','П','Р','С','Т','У','Ф','Х','Ц','Ч','Ш','Щ','Э','Й','Ы','Ь','Ю','Я' };
 	unsigned char d[256][256];
 	unsigned char newalphabet[N];
-	c = N;
-	for (i = 0; i < N; i++)
+	FILE* out = fopen("out.txt", "w");
+	
+	if (change == 1)
 	{
-		j = rand() % c;
-		newalphabet[i] = alphabet[j];
-		alphabet[j] = alphabet[c - 1];
-		c--;
+		c = N;
+		for (i = 0; i < N; i++)
+		{
+			j = rand() % c;
+
+			newalphabet[i] = alphabet[j];
+			alphabet[j] = alphabet[c - 1];
+			c--;
+		}
+		c = 0;
+		for (i = 0; i < N; i++)
+		{
+			for (j = 0; j < N; j++)
+			{
+
+				c = j + i;
+				if (c > N - 1)
+					c -= N;
+				d[savealphabet[i]][savealphabet[j]] = newalphabet[c];
+			}
+
+		}
+
+		i = 0; j = 0;
+		while (sizefile)
+		{
+			if (j == 5)
+			{
+				fputc(' ', fp2);
+				j = 0;
+			}
+			chbuf = fgetc(fp1);
+			sizefile--;
+			if (i == strlen((const char*)code))
+				i -= strlen((const char*)code);
+			if ((chbuf > 191) && (chbuf < 224))
+			{
+				chbuf = d[code[i]][chbuf];
+				i++;
+				fputc(chbuf, fp2);
+				j++;
+			}
+			if ((chbuf > 223))
+			{
+
+				chbuf = d[code[i]][chbuf - 32];
+				i++;
+				fputc(chbuf, fp2);
+				j++;
+			}
+
+
+		}
+		fprintf(out, "%s\n", code);
 	}
-	c = 0;
+	else
+	{	
+		FILE* ALPHABIT = fopen("alphabit.txt", "r");
+		fscanf(ALPHABIT, "%s", code);
+		fgetc(ALPHABIT);
+		for (i = 0; i < N; i++)
+		{
+			
+			newalphabet[i]=fgetc(ALPHABIT);
+		}
+	
+		
+		for (i = 0; i < N; i++)
+		{
+			for (j = 0; j < N; j++)
+			{
+
+				c = j + i;
+				if (c > N - 1)
+					c -= N;
+				d[savealphabet[i]][savealphabet[j]] = newalphabet[c];
+			}
+
+		}
+		i = 0; j = 0;
+		while (sizefile)
+		{
+			if (j == 5)
+			{
+				fputc(' ', fp2);
+				j = 0;
+			}
+			chbuf = fgetc(fp1);
+			sizefile--;
+			if (i == strlen((const char*)code))
+				i -= strlen((const char*)code);
+			if ((chbuf > 191) && (chbuf < 224))
+			{
+				chbuf = d[code[i]][chbuf];
+				i++;
+				fputc(chbuf, fp2);
+				j++;
+			}
+			if ((chbuf > 223))
+			{
+
+				chbuf = d[code[i]][chbuf - 32];
+				i++;
+				fputc(chbuf, fp2);
+				j++;
+			}
+
+
+		}
+
+		fprintf(out, "%s\n", code);
+
+	}
+	
 	for (i = 0; i < N; i++)
 	{
+		fputc('\n', out);
 		for (j = 0; j < N; j++)
 		{
-
-			c = j + i;
-			if (c > N - 1)
-				c -= N;
-			d[savealphabet[i]][savealphabet[j]] = newalphabet[c];
+			fputc(newalphabet[(i + j) % N], out);
 		}
-
 	}
-
-	i = 0; j = 0;
-	while (sizefile)
-	{
-		if (j == 5)
-		{
-			fputc(' ', fp2);
-			j = 0;
-		}
-		chbuf = fgetc(fp1);
-		sizefile--;
-		if (i == strlen((const char*) code))
-			i -= strlen((const char*)code);
-		if ((chbuf >191)&&(chbuf<224))
-		{
-			chbuf = d[code[i]][chbuf];
-			i++;
-			fputc(chbuf, fp2);
-			j++;
-		}
-		if ((chbuf>223))
-		{
-
-			chbuf = d[code[i]][chbuf - 32];
-			i++;
-			fputc(chbuf, fp2);
-			j++;
-		}
+	fputc('\n', out);
 	
-	
-	}
 	return 0;
 }
